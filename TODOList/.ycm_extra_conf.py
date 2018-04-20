9ad36b35dbfbabe49a3e3cee3fe17ec6a5e6b041
rhuard <rhuard13@gmail.com>
2018-04-19 22:38:10 -0700

    improve format and grab from local files now

--- TODOList/.ycm_extra_conf.py
+++ TODOList/.ycm_extra_conf.py
@@ -1,4 +1,15 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 def FlagsForFile(filename, **kwargs):
     return {
-            'flags': ['-lncurses', '-Wall', '-Wextra', '-Werror']
+            'flags': ['-x', 'c++',
+                      '-lncurses',
+                      '-Wall',
+                      '-Wextra',
+                      '-Werror',
+                      '-isystem', '/usr/include',
+                      '-I', '{}/include'.format(dir_path),
+                      '-I','{}/src'.format(dir_path),
+                      '-I', '{}'.format(dir_path)]
     }
